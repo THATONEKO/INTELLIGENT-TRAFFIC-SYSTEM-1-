2fe0f59f50e92c657515e837e12689cd7c1d8b24
THATONEKO <thatoneko03@gmail.com>
2024-08-09 12:01:21 +0200

    back end

--- web-app/src/components/Tabs.jsx
+++ web-app/src/components/Tabs.jsx
@@ -17,7 +17,7 @@ const Tabs = ({ activeTab, setActiveTab, handleThemeChange }) => {
 
   const fetchRecentLocations = async (activetabs) => {
     try {
-      const response = await axios.get(`http://192.168.1.53:5000/${activetabs}/`);
+      const response = await axios.get(`https://h27x0zfg-5000.inc1.devtunnels.ms/${activetabs}/`);
       console.log(response.data);
       setRecentLocations(response.data);
     } catch (error) {
@@ -31,7 +31,7 @@ const Tabs = ({ activeTab, setActiveTab, handleThemeChange }) => {
 
   const handleDelete = async (location) => {
     try {
-      await axios.delete(`http://192.168.1.53:5000/${activeTab}/${location}`); // Replace with your Flask backend URL
+      await axios.delete(`https://h27x0zfg-5000.inc1.devtunnels.ms/${activeTab}/${location}`); // Replace with your Flask backend URL
       fetchRecentLocations(activeTab); // Refresh the list after deletion
     } catch (error) {
       console.error('Error deleting location:', error);
